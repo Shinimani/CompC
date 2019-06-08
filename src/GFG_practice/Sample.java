174cf09adf91dad6d41c9de5c0b90bcd5fec4919
Aayush <aayush.somani111@gmail.com>
2019-06-08 19:49:12 +0530

    Sample code to copy

---
+++ src/GFG_practice/Sample.java
@@ -0,0 +1,13 @@
+package GFG_practice;
+
+public class Sample {
+    public static void main(String[] args) throws Exception {
+        FastScanner f = new FastScanner();
+        int T = f.nextInt();
+        while (T-- > 0) {
+            int N = f.nextInt();
+            int[] a = f.nextIntArray(N);
+
+        }
+    }
+}
