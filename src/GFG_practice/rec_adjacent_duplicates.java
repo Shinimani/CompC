82848969bcf04485dc46cbd76e637bf26327ba4b
Aayush <aayush.somani111@gmail.com>
2019-06-16 03:01:48 +0530

    Started the code

---
+++ src/GFG_practice/rec_adjacent_duplicates.java
@@ -0,0 +1,22 @@
+package GFG_practice;
+
+public class rec_adjacent_duplicates {
+//    https://practice.geeksforgeeks.org/problems/recursively-remove-all-adjacent-duplicates/0
+
+    public static void main(String[] args) throws Exception {
+        FastScanner f = new FastScanner();
+        int T = f.nextInt();
+        while (T-- > 0) {
+//            int N = f.nextInt();
+//            int[] a = f.nextIntArray(N);
+            while(f.hasNext())
+            {
+
+            }
+
+        }
+    }
+
+
+}
+
