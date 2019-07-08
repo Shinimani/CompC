bc9bd58387f3eeb425f211916f7d63f148b358b1
Aayush <aayush.somani111@gmail.com>
2019-07-08 09:39:07 +0530

    Sample for codeforces

---
+++ src/Codeforces/cf_single_sample.java
@@ -0,0 +1,10 @@
+package Codeforces;
+
+public class cf_single_sample {
+    public static void main(String[] args) {
+        FastScanner f = new FastScanner();
+        int n = f.nextInt();
+        int[] a = f.nextIntArray(n);
+
+    }
+}
