ab70443026030b73a1afe5e48bb994458279cec6
Aayush <aayush.somani111@gmail.com>
2019-08-01 07:46:54 +0530

    To look at solution again

--- src/Interview_bit/Mathss.java
+++ src/Interview_bit/Mathss.java
@@ -145,15 +145,37 @@ public class Mathss {
         return 0;
     }
 
-    public int solve(ArrayList<Integer> a, int b, int c) {
-        for (int i = 0; i < a.size(); i++) {
-
+    //    https://www.interviewbit.com/problems/numbers-of-length-n-and-value-less-than-k/
+    public int solve(ArrayList<Integer> A, int B, int C) {
+        int cSize = (int) Math.log10(C) + 1;
+        int n = A.size();
+        if (cSize < B || n == 0) return 0;
+        boolean hasZero = A.get(0) == 0;
+        if (cSize > B) return (B > 1 && hasZero ? n - 1 : n) * (int) Math.pow(n, B - 1);
+        // B == cSize
+        int pow10 = (int) Math.pow(10, B - 1), count = 0;
+        for (int i = B; i > 0; i--) {
+            int target = C / pow10, j;
+            C %= pow10;
+            pow10 /= 10;
+            for (j = 0; j < n; j++) {
+                if (A.get(j) >= target) break;
+            }
+            count += (B > 1 && i == B && hasZero ? j - 1 : j) * (int) Math.pow(n, i - 1);
+            if (j == n || A.get(j) > target) break;
         }
-
-        return 0;
-
-
+        return count;
     }
+
+
+    /*
+    int nlesskhelper (ArrayList<Integer> a, int lengthh, int[] c)
+    {
+        int ans = 0;
+
+        return ans;
+
+    }*/
 
 
 
