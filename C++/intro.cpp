7c84e095ae23cee8d76bda0fafb702fecba1f12e
Aayush <aayush.somani111@gmail.com>
2019-12-04 13:27:40 +0530

    introduction going on

--- C++/intro.cpp
+++ C++/intro.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+
+
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Note that the newline "\n" works faster than endl, because endl always
+    // causes a flush operation.
+
     cout<<"Hello world"<<endl;
+    // int a, b;
+    // scanf("%d %d", &a, &b);
+    // printf("%d and %d\n",a,b);
+    // string st;
+    // getline(cin, st);
+    // cout<<st;
+    int a = 123456789;
+    long long b = a*a;
+    cout << b << "\n"; // -1757895751
+
+    b = (long long) a*a;
+    cout << b << "\n"; // correct answer
 }
